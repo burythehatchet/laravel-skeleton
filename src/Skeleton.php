941e433df1fc3728092b32a2ee84b39ac4882f13
Bruno Gaspar <brunofgaspar1@gmail.com>
2016-06-06 13:04:02 +0100

    Add base class and test.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

---
+++ src/Skeleton.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Werxe\:Package;
+
+class :Package
+{
+    /**
+     * Constructor.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+}
---
+++ tests/SampleTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Werxe\:Package\Tests;
+
+use PHPUnit_Framework_TestCase;
+
+class SampleTest extends PHPUnit_Framework_TestCase
+{
+    /** @test */
+    public function true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
