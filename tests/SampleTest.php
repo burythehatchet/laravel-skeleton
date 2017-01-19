870023c1c059c8d3a03e4994c02a13e68921bc6b
Bruno Gaspar <brunofgaspar1@gmail.com>
2017-01-19 00:06:48 +0000

    Add copyright header to files.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Skeleton package.
+ *
+ * (c) Bruno Gaspar <bruno@werxe.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the LICENSE file.
+ */
+
 namespace Werxe\Skeleton\Tests;
 
 use PHPUnit_Framework_TestCase;
