3181f70d3b768312e1049d0e5498e34fb18f8a6e
Bruno Gaspar <brunofgaspar1@gmail.com>
2017-08-25 04:13:01 +0100

    chore: Remove copyright headers.
    Signed-off-by: Bruno Gaspar <brunofgaspar1@gmail.com>

--- src/Skeleton.php
+++ src/Skeleton.php
@@ -1,14 +1,5 @@
 <?php
 
-/*
- * This file is part of the Skeleton package.
- *
- * (c) Werxe <hello@werxe.com>
- *
- * For the full copyright and license information, please refer to
- * the LICENSE file that was distributed with this source code.
- */
-
 namespace Werxe\Skeleton;
 
 class Skeleton
